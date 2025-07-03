1ca15c3678bad7c65026b5c87487a37699934604
DollarforSlotLLC <s2wcgpt@gmail.com>
2025-07-02 21:18:49 -0400

    Update create-checkout-session.js

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+console.log("🔐 Stripe Secret Key:", process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
